246e42a6efcac45f47046d9acc5be284a6afc2b2
L Rayo <rayo862@hotmail.com>
2023-04-29 20:20:03 -0500

    url cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/CV.pdf';
+
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={ CV } download className='btn'>Download CV</a>
+        <a href='https://drive.google.com/file/d/1o3brunhIAGaI46K1CyShuacGWuq-iDTU/view?usp=share_link' download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
